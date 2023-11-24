61f6fd0d551cbe6e63472de0d77cb1b9c65badc4
Stepafn <138586684+Stepafn@users.noreply.github.com>
2023-11-24 20:09:53 +0100

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
 GIT_REPO_URL="https://github.com/Stepafn/bot.py.git"
 GIT_REPO_NAME="bot.py"
 DOCKER_IMAGE_NAME="tele-bot:1.0"
 DOCKER_REGISTRY_USERNAME="stepafn"
 DOCKER_REGISTRY_PASSWORD="aA151104!"
-DOCKER_REGISTRY_URL="my-private-registry.com:5000"
+DOCKER_REGISTRY_URL="localhost:5000"
 
-git clone $GIT_REPO_URL --branch bot
+git clone $GIT_REPO_URL
 
 cp config.env $GIT_REPO_NAME/config.env
 cd bot.py
